be516a1c4b8274f2aa71523cd574161525649991
Taher <taherc.ali@gmail.com>
2024-10-27 11:48:44 +0530

    reverse

---
+++ src/main/java/sort/ReverseString.java
@@ -0,0 +1,50 @@
+package sort;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		String string = "Taher";
+		reverseString(string);
+
+		string = "Taher is good employee";
+		reverseString(string);
+
+		reverseEachWordInString(string);
+	}
+
+	private static void reverseString(String string) {
+		System.out.println("original string : " + string);
+		char[] ch = string.toCharArray();
+
+		StringBuilder reverseString = new StringBuilder();
+
+		for (int i = ch.length - 1; i >= 0; i--) {
+			reverseString.append(ch[i]);
+		}
+		System.out.println("reverse string : " + reverseString);
+		System.out.println();
+	}
+
+	private static void reverseEachWordInString(String string) {
+		System.out.println("original string : " + string);
+		String[] words = string.split(" ");
+
+		StringBuilder reverseString = new StringBuilder();
+
+		int counter = 0;
+
+		for (String word : words) {
+			counter++;
+			char[] ch = word.toCharArray();
+			for (int i = ch.length - 1; i >= 0; i--) {
+				reverseString.append(ch[i]);
+			}
+			if (counter < words.length) {
+				reverseString.append(" ");
+			}
+		}
+		System.out.println("reverse each word in string : " + reverseString);
+	}
+
+}

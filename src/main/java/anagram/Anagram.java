9405a519972f7bfa95ed6c5667766520a840ba3a
Taher <taherc.ali@gmail.com>
2024-11-01 07:16:14 +0530

    anagram

--- src/main/java/anagram/Anagram.java
+++ src/main/java/anagram/Anagram.java
@@ -5,9 +5,9 @@ public class Anagram {
 	public static void main(String[] args) {
 
 		String str1 = "taher";
-		String str2 = "rehat";
-		
-		if(isAnagram(str1, str2)) {
+		String str2 = "rehasa";
+
+		if (isAnagram(str1, str2)) {
 			System.out.println(str1 + " and " + str2 + " are anagram");
 		} else {
 			System.out.println(str1 + " and " + str2 + " are not anagram");
@@ -19,38 +19,31 @@ public class Anagram {
 		if (str1.length() != str2.length()) {
 			return false;
 		}
-
 		str1 = sortString(str1);
 		str2 = sortString(str2);
-		
-		//a e h r t
-		
 		char[] ch1 = str1.toCharArray();
 		char[] ch2 = str2.toCharArray();
-		
-		for (int i = 0; i < ch1.length; i++) {
-			if(ch1[i]!=ch2[i]) {
+		for (int i = 0, j = 0; i < ch1.length; i++, j++) {
+			if (ch1[i] != ch2[j]) {
 				return false;
 			}
 		}
-
 		return true;
 	}
 
 	private static String sortString(String string) {
 		char[] ch = string.toCharArray();
-		char temp;
 		for (int i = 0; i < ch.length; i++) {
+			char temp;
 			for (int j = i + 1; j < ch.length; j++) {
 				if (ch[i] > ch[j]) {
-					temp = ch[j];
-					ch[j] = ch[i];
-					ch[i] = temp;
+					temp = ch[i];
+					ch[i] = ch[j];
+					ch[j] = temp;
 				}
 			}
 		}
-		String sortedString = new String(ch);
-		return sortedString;
+		return new String(ch);
 	}
 
 }
